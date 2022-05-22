18a5b35e1e2f99538309798d13c2a56f32f1ca7a
saintyulo09 <100059944+saintyulo09@users.noreply.github.com>
2022-05-22 10:33:37 +0800

    Add files via upload

---
+++ CLE1/about.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>My Portfolio</title>
+        <link rel="stylesheet" type="text/css"
+         href="css/stylesheet.css"/> <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap">
+    
+    </head>
+<body background="images/last4.jpg">
+        <div id="wrapper">
+            <div id="logo-menu">
+                <div id="logo">
+                    <a href="index.php"><img src="images/logo2.png" width="300" height="150" 
+                    title="Logo" alt="Logo" /></a>
+                </div>
+                
+                <div id="menu">
+                    <ul>
+                        <li><a href="home.php">HOME</a></li>
+                        <li><a href="about.php">ABOUT</a></li>
+                        <li><a href="index.php">WORK</a></li>
+                        
+                    </ul>
+                
+                </div>
+            </div>
+            <div id="contents">
+            <div id="pic-content">
+                <div id="pic6">
+                    <img src="images/palm1.png" width="240" height="300" 
+                    title="palm" alt="palm" />
+                        
+            </div>
+                <div id="pic10">
+                    <img src="images/pp4.png" width="450" height="800" 
+                    title="pic" alt="pic" />
+                        
+            </div>
+                <div id="text5"> 
+                <p>NAME: Saint Yulo</p>
+                <p>AGE: 19</p>
+                </div>
+                <div id="text7"> 
+                <p>- DJ</p>
+                <p>- GAMER</p>
+                <p>- IT STUDENT</p>
+                <p>- BASKETBALL PLAYER</p>
+                </div>
+                <div id="text8"> 
+                <p>Web design can be great for creative types because it allows you to create something both beautiful and functional, all from your computer. If you are looking for a way to use your artistic side, web design is a great way to do it. In today's world, learning how to design websites can be an incredibly useful skill when you are a web developer.</p>
+                </div>
+                    
+       </div>
+    </div>
+</div>
+</body>
+</html> 
---
+++ CLE1/home.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>My Portfolio</title>
+        <link rel="stylesheet" type="text/css"
+         href="css/stylesheet.css"/> <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap">
+    
+    </head>
+<body background="images/last4.jpg">
+        <div id="wrapper">
+            <div id="logo-menu">
+                <div id="logo">
+                    <a href="index.php"><img src="images/logo2.png" width="300" height="150" 
+                    title="Logo" alt="Logo" /></a>
+                </div>
+                
+                <div id="menu">
+                    <ul>
+                        <li><a href="home.php">HOME</a></li>
+                        <li><a href="about.php">ABOUT</a></li>
+                        <li><a href="index.php">WORK</a></li>
+                        
+                    </ul>
+                
+                </div>
+            </div>
+            <div id="contents">
+            <div id="pic-content">
+                <div id="pic6">
+                    <img src="images/palm1.png" width="240" height="300" 
+                    title="palm" alt="palm" />
+                        
+            </div>
+                <div id="pic9">
+                    <img src="images/pp3.png" width="400" height="800" 
+                    title="pic" alt="pic" />
+                        
+            </div>
+                 <div id="texts">
+                <p>Hi, I'm Saint Yulo.</p>
+                     
+           </div>
+                <div id="texts2">
+                <p>Welcome to my World.</p>
+                     
+           </div>
+                    
+       </div>
+    </div>
+</div>
+</body>
+</html> 
